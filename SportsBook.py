9132b90a0dc767a1ffe27c9deaf4daec5a662b4c
David Bristoll <36759641+Pringleman83@users.noreply.github.com>
2018-09-14 22:18:10 +0100

    Changed the sportsBook file to the main menu
    This is the new main menu system. It shouldn't need any work for a while as it does everything I wanted it to do. 
    It will have to import the main function for each additional sport (like I said, a while away!).
    
    The previous football script is now football.py.

---
+++ SportsBook.py
@@ -0,0 +1,65 @@
+from football import footballMain
+
+def isNumber(s):
+    """
+    Tests if the value passed is a number.
+    Returns True or False.
+    """
+    try:
+        float(s)
+        return True
+    except ValueError:
+        pass
+ 
+    try:
+        import unicodedata
+        unicodedata.numeric(s)
+        return True
+    except (TypeError, ValueError):
+        pass
+ 
+    return False
+
+
+def tennis():
+    """
+    Placeholder for Tennis selection.
+    Serves no function except for menu testing.
+    """
+    print("Tennis selected")
+
+# options list can only now be declared (after its functions have been)
+options = [[1, "Football", footballMain],[2, "Tennis", tennis], ["Q", "Quit"]]
+
+def mainMenu(options):
+    """
+    Main menu function
+    Offers all options in the options list (no need to edit this function)
+    Returns the selected funtion to run.
+    """
+    while True:
+        print("Welcome to SportsBook - A sports analysis tool.")
+        print("Please select from one of the following sports:")
+        # Display all options
+        for option in options:
+            print(str(option[0]) + " " + str(option[1]))
+        # Take user selection
+        while True:
+            print("Enter option: ")
+            selected = input()
+            # Quit option
+            if selected.lower() == "q":
+                exit()
+            # If a number has been entered, convert the string to an integer
+            if isNumber(selected):
+                selected = int(selected)
+                # If the selection is in the list break out of the infinite loop
+                if selected in range(len(options)) and selected != 0:
+                    break
+        # Having broken out of the loop, run the selected function
+        for option in options:
+            if selected == option[0]:
+                option[2]()
+
+# As this is the main file, call up the main menu    
+mainMenu(options)
