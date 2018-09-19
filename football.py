1e0488a34ed037fcffbb5a578360d582c5bfd39c
David Bristoll <36759641+Pringleman83@users.noreply.github.com>
2018-09-19 17:20:19 +0100

    Updated file
    I've made some big changes here. The file now integrates nicely with the menu system. I've also removed any unintended recursion. The functions also now look and work more like functions should. 
    I've also added some input validation to prevent errors and tidied the code somewhat.

--- football.py
+++ football.py
@@ -2,17 +2,8 @@ from bs4 import BeautifulSoup as soup
 from urllib.request import urlopen as uReq
 import pprint
 import json
-
-def footballMain():
-    """The first called function.
-    Currently calls the only option available, the selectLeague() option.
-    Needs building up to offer the selection of multiple options.
-    """
-    selectedLeague = selectLeague()
-
-    displaySelection(selectedLeague)
     
-def selectLeague():
+def selectLeague(leagueData):
     """Takes  in the availableLeagues dictionary.
     Prompts the user to select a league.
     Returns the key value of theselected league.
@@ -21,42 +12,57 @@ def selectLeague():
     This is the acceptance of options 97-100.
     This function needs to be built into a menu.
     """
+    while True:
+        availableOptions = []
+        option = "" #Number entered by user
+        selection = "" #League the option relates to
     
-    print("\n Select a league number to study: \n ")
+        print("\n Select a league to add: \n ")
 
-    for key in availableLeagues:
-        print(key)
+        # Display the leagues available and create the availableOptions list of availble
+        #option numbers for input validation later on.
+        for league in availableLeagues:
+            print(league)
+            availableOptions.append(availableLeagues[league][0])
 
-    leagueSelector = int(input())
-    selectedLeague = ""
-    for key in availableLeagues:
-        if leagueSelector == availableLeagues[key][0]:
-            selection = key
+        # Debug code: view the availableOptions list after it is created.
+        #print("Available options:")
+        #pprint.pprint(availableOptions)
 
-    if selection == "":
-        print("Invalid selection")
-        selectLeague()
+        # While no valid option has been entered, wait for a valid option
+        #(the earlier described input validation)
+        while option not in availableOptions:
+            option = input()
+
+        # Assign the league name to the selection variable
+        for league in availableLeagues:
+            if option == availableLeagues[league][0]:
+                selection = league
+                #Debug code: Display selected league string
+                print("Selected league" + league)
         
-    # Extra function as described in docstring
-    elif selection == "99 Export JSON":
-        exportJSONFile()
-        footballMain()
-    elif selection == "98 Import JSON":
-        importJSONFile()
-        footballMain()
-    elif selection == "97 Display Currently Loaded Data":
-        pprint.pprint(leagueData)
-        input("Press enter to continue")
-        footballMain()
-    elif selection == "100 Quit":
-        exit()
-    # End of extra function
-    
-    else:
-        return selection
-    footballMain()
+        # Extra function as described in docstring
+        if selection == "99 Export JSON":
+            exportJSONFile(leagueData)
+            
+        elif selection == "98 Import JSON":
+            leagueData = importJSONFile(leagueData)
+            
+        elif selection == "97 Display Currently Loaded Data":
+            pprint.pprint(leagueData)
+            input("Press enter to continue")
 
-def importJSONFile():
+        elif selection == "100 Quit":
+            #Debug code: indicate selection:
+            print("Option 100 selected")
+            return leagueData
+        # End of extra function
+
+        else:
+            leagueData = displaySelection(selection, leagueData)
+
+
+def importJSONFile(leagueData):
     """ Loads the leagueData.json file into the leagueData dictionary.
     Needs to return the file rather than all be actioned in the function.
     This can only be done properly once the menu system is in place because
@@ -64,15 +70,15 @@ def importJSONFile():
     function. This would mean passing the leagueData dictionary down through
     multiple functions rather than directly to this function.
     """
-    global leagueData
+    #global leagueData
     print("---LOADING...---")
     with open("leagueData.json") as infile:
         leagueData = json.load(infile)
     print("---LOADED---")
     input("Press enter to continue")
-    footballMain()
+    return leagueData
         
-def exportJSONFile():
+def exportJSONFile(leagueData):
     """ Saves the leagueData dictionary to a json file called
     leagueData.json.
     Needs to return the file rather than all be actioned in the function.
@@ -87,22 +93,30 @@ def exportJSONFile():
     print("---SAVED---")
     input("Press enter to continue")
 
-def displaySelection(selection):
+def displaySelection(selection, leagueData):
     """ Takes in the key value of the selected league.
     Prints the league name and number of teams in that league.
     Gives the option of confirming the selection of that league or returning
     to the main menu.
     """
+    choice = ""
     print("You selected " + selection + "\n")
-    print("This will use the following url: " + availableLeagues[selection][1] + "\n")
+    
+    # Debug code: display the URL that will be used to obtain the league data
+    #print("This will use the following url: " + availableLeagues[selection][1] + "\n")
+    
     print("The league has " + str(availableLeagues[selection][2]) + " teams.")
-    choice = int(input("Press 1 to generate this league table or 2 to go back to the main menu."))
-    if choice == 1:
-        gatherLeagueInfo(selection)
-    else:
-        footballMain()
 
-def gatherLeagueInfo(selection):
+    while choice != "1" or choice != "2":
+        choice = input("Type 1 to gather this data or 2 to go back to the main menu.")
+        if choice == "1":
+            getLeagueData(selection, leagueData)
+            return leagueData
+        elif choice == "2":
+            return leagueData
+
+
+def getLeagueData(selection, leagueData):
     """ Takes the key of the selected league from the availableLeagues dictionary.
     Scrapes the selected league information from bedstudy.com.
     Calculates unscraped data (for example, total games won).
@@ -172,32 +186,36 @@ def gatherLeagueInfo(selection):
             }
 
     input("Press enter to continue.")
-    footballMain()
+    return leagueData
 
 # The availableLeagues dictionary: "League name":["Option number", "League link from betstudy.com", "Number of teams in league"]   
-availableLeagues = {"1 English Premier League":[1, "england/premier-league/", 20],
-                    "2 English Championship":[2, "england/championship/", 24],
-                    "3 English League One":[3, "england/league-one/", 24],
-                    "4 English League Two":[4, "england/league-two/", 24],
-                    "5 Spanish Primera":[5, "spain/primera-division/", 20],
-                    "6 Spanish Segunda":[6, "spain/segunda-division/", 22],
-                    "7 Spanish Segunda B":[7, "spain/segunda-b/", 20],
-                    "8 French Lique 1":[8, "france/ligue-1/", 20],
-                    "9 French Ligue 2":[9, "france/ligue-2/", 20],
-                    "10 German Bundesliga":[10, "germany/bundesliga/", 18],
-                    "11 German 2 Bundesliga":[11, "germany/2.-bundesliga/", 18],
-                    "12 German Liga":[12, "germany/3.-liga/", 20],
-                    "13 Italian Serie A":[13, "italy/serie-a/", 20],
-                    "14 Italian Serie B":[14, "italy/serie-b/", 19],
-                    "15 Brazillian Serie A":[15, "brazil/serie-a/", 20],
-                    "16 Brazillian Serie B":[16, "brazil/serie-b/", 20],
-                    "17 Argentinian Primera Division":[17, "argentina/primera-division/", 26],
-                    "18 Argentinian Prim B Nacional":[18, "argentina/prim-b-nacional/", 25],
-                    "19 Argentinian Prim B Metro":[19, "argentina/prim-b-metro/", 20],
-                    "97 Display Currently Loaded Data":[97, "", 0],
-                    "98 Import JSON":[98, "", 0],
-                    "99 Export JSON":[99, "", 0],
-                    "100 Quit":[100, "", 0]
+availableLeagues = {"1 English Premier League":["1", "england/premier-league/", 20],
+                    "2 English Championship":["2", "england/championship/", 24],
+                    "3 English League One":["3", "england/league-one/", 24],
+                    "4 English League Two":["4", "england/league-two/", 24],
+                    "5 Spanish Primera":["5", "spain/primera-division/", 20],
+                    "6 Spanish Segunda":["6", "spain/segunda-division/", 22],
+                    "7 Spanish Segunda B":["7", "spain/segunda-b/", 20],
+                    "8 French Lique 1":["8", "france/ligue-1/", 20],
+                    "9 French Ligue 2":["9", "france/ligue-2/", 20],
+                    "10 German Bundesliga":["10", "germany/bundesliga/", 18],
+                    "11 German 2 Bundesliga":["11", "germany/2.-bundesliga/", 18],
+                    "12 German Liga":["12", "germany/3.-liga/", 20],
+                    "13 Italian Serie A":["13", "italy/serie-a/", 20],
+                    "14 Italian Serie B":["14", "italy/serie-b/", 19],
+                    "15 Brazillian Serie A":["15", "brazil/serie-a/", 20],
+                    "16 Brazillian Serie B":["16", "brazil/serie-b/", 20],
+                    "17 Argentinian Primera Division":["17", "argentina/primera-division/", 26],
+                    "18 Argentinian Prim B Nacional":["18", "argentina/prim-b-nacional/", 25],
+                    "19 Argentinian Prim B Metro":["19", "argentina/prim-b-metro/", 20],
+                    "20 Scottish Premier":["20", "scotland/premiership/", 12], #check
+                    "21 Scottish Championship":["21", "scotland/championship/", 10], #check
+                    "22 Scottish League One":["22", "scotland/league-one/", 10], #check
+                    "23 Scottish League Two":["23", "scotland/league-two/", 10], #check
+                    "97 Display Currently Loaded Data":["97", "", 0],
+                    "98 Import JSON":["98", "", 0],
+                    "99 Export JSON":["99", "", 0],
+                    "100 Quit":["100", "", 0]
                     }
 
 def getLeague(t, leagueData):
@@ -291,6 +309,3 @@ def compare(homeTeam, awayTeam, leagueData):
     comparison = [homeAwayDifference, totalDifference]
     
     return comparison
-
-# Initialise the leagueData dictionary
-leagueData = {}
