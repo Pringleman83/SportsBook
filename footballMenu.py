72bc5e8e56c1a23d8c9e6dfc222e5604143d57a6
David Bristoll <36759641+Pringleman83@users.noreply.github.com>
2018-09-24 12:38:39 +0100

    Update footballMenu.py
    Added author information.
    
    Added the reports sub menu.
    
    Moved options: import, export and display leagues from the leagueSelect menu to the footballMenu and reports sub ment.
    
    Removed leagueData dictionary initialisation (This now takes place in the main sportsBook.py file).
    
    Added an option to clear the loaded leagueData dictionary.
    
    Removed "*"s from any available options.

--- footballMenu.py
+++ footballMenu.py
@@ -1,9 +1,13 @@
 #Football menu system
 from commonFunctions import isNumber
 from football import *
+import pprint
 
-# Initialise the leagueData dictionary
-leagueData = {}
+__author__ = "David Bristoll"
+__copyright__ = "Copyright 2018, David Bristoll"
+__maintainer__ = "David Bristoll"
+__email__ = "david.bristoll@gmail.com"
+__status__ = "Development"
 
 # temporary, placeholder functions:
 
@@ -22,22 +26,64 @@ def displayAnalysis(x):
 def singleGameAnalysis(x):
     print("\nThe single game analysis feature is not yet available.\n")
 
-def reports(x):
-    print("\nThe reports feature is not yet available.\n")
-
 def leave(x):
-    print("\nExit to main menu.\n")
+    print("\nExit to previous menu.\n")
+    
+def chooseLeagues(leagueData):
+    leagueData = selectLeague(leagueData)
 
-def footballMenu():
-    footballOptions = [["(1) League select", "1", selectLeague],#The selectLeague function from football.py
+def displaySelectedLeagues(leagueData):
+    pprint.pprint(leagueData)
+
+def reports(leagueData):
+    print("\nReports Menu\n")
+    
+    reportOptions = [["(1) Export JSON data", "1"],
+                    ["(2) Display currently loaded league data", "2"],
+                    ["(M) Return to previous menu", "m"]
+                    ]
+    
+    exitMenu = False
+    availableOptions = []
+    selection = ""
+    
+    # Gather a list of availableOption numbers for input recognition
+    for option in reportOptions:
+        availableOptions.append(option[1])
+    
+    while exitMenu == False:
+        
+        selectedLeagues = []
+        
+        for option in reportOptions:
+            print(option[0])
+            
+        while selection not in availableOptions:
+            selection = input()
+            selection = selection.lower()
+        
+        # Menu selection conditionals
+        if selection.lower() == "m":
+                exitMenu = True
+                return leagueData
+        if  selection == reportOptions[0][1]:
+            exportJSONFile(leagueData)
+        if selection == reportOptions[1][1]:
+            displaySelectedLeagues(leagueData)
+        selection = ""
+
+def footballMenu(leagueData):
+    footballOptions = [["(1) League select", "1", chooseLeagues],#The selectLeague function from football.py
                    ["(2) Download upcoming fixtures*", "2", downloadFixtures],
                    ["(3) Display upcoming fixtures*", "3", displayFixtures],
                    ["(4) Run analytics on upcoming fixtures*", "4", analyseFixtures],
                    ["(5) Display analytics in upcoming fixtures*", "5", displayAnalysis],
                    ["(6) Single game analysis from fixture list*", "6", singleGameAnalysis],
                    ["(7) Manual single game analysis", "7", manualGameAnalysis],
-                   ["(8) Reports*", "8", reports],
-                   ["(M) Main menu*", "m", leave]
+                   ["(8) Reports", "8", reports],
+                   ["(9) Import data from JSON file", "9"],
+                   ["(10) Clear currently loaded league data", "10"],
+                   ["(M) Previous menu", "m", leave]
                    ]
     selectedLeagues = []
     exitMenu = False
@@ -82,6 +128,14 @@ def footballMenu():
             if selection.lower() == "m":
                 exitMenu = True
                 break
+            if selection == "9":
+                leagueData = importJSONFile()
+                selection = ""
+                continue
+            if selection == "10":
+                leagueData = {}
+                selection = ""
+                continue
             if selection == option[1]:
                 option[2](leagueData)
                 selection = ""
