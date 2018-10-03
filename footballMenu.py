4169751a0db07c5202a3cebb8e8e79dc558ff1e3
David Bristoll <36759641+Pringleman83@users.noreply.github.com>
2018-10-03 01:11:45 +0100

    Update footballMenu.py
    Tidied some menu behaviours.

--- footballMenu.py
+++ footballMenu.py
@@ -1,4 +1,5 @@
 #Football menu system
+from commonFunctions import is_number
 from football import *
 import pprint
 
@@ -12,187 +13,198 @@ predictions = []
 
 # temporary, placeholder functions:
 
-def downloadFixtures(x):
+
+def download_fixtures(x):
     print("\nThe download fixtures feature is not yet available.\n")
 
-def displayFixtures(x):
+
+def display_fixtures(x):
     print("\nThe display fixtures feature is not yet available.\n")
 
-def analyseFixtures(x):
+
+def analyse_fixtures(x):
     print("\nThe analyse fixtures feature is not yet available.\n")
 
-def displayAnalysis(x):
+
+def display_analysis(x):
     print("\nThe display analysis feature is not yet available.\n")
 
-def singleGameAnalysis(x):
+
+def single_game_analysis(x):
     print("\nThe single game analysis feature is not yet available.\n")
+
 
 def leave(x):
     print("\nExit to previous menu.\n")
-    
-def chooseLeagues(leagueData):
-    leagueData = selectLeague(leagueData)
 
-def displaySelectedLeagues(leagueData):
-    pprint.pprint(leagueData)
 
-def displayPredictions(predictions):
+def choose_leagues(league_data):
+    league_data = select_league(league_data)
+
+
+def display_selected_leagues(league_data):
+    pprint.pprint(league_data)
+
+
+def display_predictions(predictions):
     """
     Takes in the current list of predictions generated via game analysis options.
     Displays the predictions on screen.
     """
-    if predictions == []:
+
+    if not predictions:
         print("\nNo predictions to display. Run manual game analysis and select games to predict first.")
-        print("\nPress enter to return to previous menu.\n")
+
+        print("\nPress enter to return to previous menu.")
         input()
         return
     else:
         print("\nPredictions")
         print("===========\n")
         for game in predictions:
-            print(game[0],game[1],game[2],game[3])
+            print(game[0], game[1], game[2], game[3])
         print("\nPress enter to return to previous menu.\n")
         input()
         return
-        
-def reports(leagueData, predictions):
-    print("\nReports Menu")
+
+def reports(league_data, predictions):
+    print("\nReports Menu\n")
     print("============")
     
-    reportOptions = [["(1) Export JSON data", "1"],
-                    ["(2) Display currently loaded league data", "2"],
-                    ["(3) Display predictions", "3"],
-                    ["(M) Return to previous menu", "m"]
-                    ]
+    report_options = [["(1) Export JSON data", "1"],
+                      ["(2) Display currently loaded league data", "2"],
+                      ["(3) Display predictions", "3"],
+                      ["(M) Return to previous menu", "m"]
+                      ]
     
-    exitMenu = False
-    availableOptions = []
+    exit_menu = False
+    available_options = []
     selection = ""
     
-    # Gather a list of availableOption numbers for input recognition
-    for option in reportOptions:
-        availableOptions.append(option[1])
+    # Gather a list of available_option numbers for input recognition
+    for option in report_options:
+        available_options.append(option[1])
     
-    while exitMenu == False:
+    while not exit_menu:
         
-        selectedLeagues = []
+        selected_leagues = []
         
-        for option in reportOptions:
+        for option in report_options:
             print(option[0])
             
-        while selection not in availableOptions:
+        while selection not in available_options:
             selection = input()
             selection = selection.lower()
         
         # Menu selection conditionals
         if selection.lower() == "m":
-                exitMenu = True
-                return leagueData
-        if  selection == reportOptions[0][1]:
-            exportJSONFile(leagueData)
-        if selection == reportOptions[1][1]:
-            displaySelectedLeagues(leagueData)
-        if selection == reportOptions[2][1]:
-            displayPredictions(predictions)
+                exit_menu = True
+                return league_data
+        if selection == report_options[0][1]:
+            export_json_file(league_data)
+        if selection == report_options[1][1]:
+            display_selected_leagues(league_data)
+        if selection == report_options[2][1]:
+            display_predictions(predictions)
         selection = ""
 
-def footballMenu(leagueData):
-    footballOptions = [["(1) Select a league", "1", chooseLeagues],#The selectLeague function from football.py
-                   ["(2) Download upcoming fixtures*", "2", downloadFixtures],
-                   ["(3) Display upcoming fixtures*", "3", displayFixtures],
-                   ["(4) Run analytics on upcoming fixtures*", "4", analyseFixtures],
-                   ["(5) Display analytics in upcoming fixtures*", "5", displayAnalysis],
-                   ["(6) Single game analysis from fixture list*", "6", singleGameAnalysis],
-                   ["(7) Manual single game analysis", "7", manualGameAnalysis],
-                   ["(8) Reports", "8", reports],
-                   ["(9) Import data from JSON file", "9"],
-                   ["(10) Clear currently loaded league data", "10"],
-                   ["(M) Previous menu", "m", leave]
-                   ]
-    selectedLeagues = []
-    exitMenu = False
-    availableOptions = []
+
+def football_menu(league_data):
+    football_options = [["(1) Select a league", "1", choose_leagues],  # The selectLeague function from football.py
+                        ["(2) Download upcoming fixtures*", "2", download_fixtures],
+                        ["(3) Display upcoming fixtures*", "3", display_fixtures],
+                        ["(4) Run analytics on upcoming fixtures*", "4", analyse_fixtures],
+                        ["(5) Display analytics in upcoming fixtures*", "5", display_analysis],
+                        ["(6) Single game analysis from fixture list*", "6", single_game_analysis],
+                        ["(7) Manual single game analysis", "7", manual_game_analysis],
+                        ["(8) Reports", "8", reports],
+                        ["(9) Import data from JSON file", "9"],
+                        ["(10) Clear currently loaded league data", "10"],
+                        ["(M) Previous menu", "m", leave]
+                        ]
+    selected_leagues = []
+    exit_menu = False
+    available_options = []
     selection = ""
 
     # Gather a list of availableOption numbers for input recognition
-    for option in footballOptions:
-        availableOptions.append(option[1])
+    for option in football_options:
+        available_options.append(option[1])
     
-    while exitMenu == False:
-        #Change the wording of the first option if a league is already selected.
-        if leagueData == {}:
-            footballOptions[0][0] = "(1) Select a league"
+    while not exit_menu:
+        if league_data == {}:
+            football_options[0][0] = "(1) Select a league"
         else:
-            footballOptions[0][0] = "(1) Select another league"
+            football_options[0][0] = "(1) Select another league"
         
-        selectedLeagues = []
+        selected_leagues = []
 
-        #If there are leages selected in LeagueData, add them to the selectedLeagues
-        #list and display the list.
-        if leagueData != {}:
-            for league in leagueData:
-                selectedLeagues.append(league)
-            print("\nSelected league(s):\n" )
-            for league in selectedLeagues:
+        #  If there are leagues selected in LeagueData, add them to the selectedLeagues
+        #  list and display the list.
+        if league_data != {}:
+            for league in league_data:
+                selected_leagues.append(league)
+            print("\n Selected league(s):\n")
+            for league in selected_leagues:
+
                 print(league)
             print()
         else:
             print("\nNo league currently selected. Please start by selecting a league.\n")
 
         # Display the available options 
-        for option in footballOptions:
+        for option in football_options:
             print(option[0])
             
         # Display any additional information
         print("\nItems marked with a * are not available in this version.")
 
         # Keep asking for a selection while the selection provided is not in the availableOptions list.
-        while selection not in availableOptions:
+        while selection not in available_options:
             selection = input()
             selection = selection.lower()
 
-        # If the seleciton is in the list, check if it is "m". If it's not, run it's function,
-        #passing the leagueData dictionary.
-        for option in footballOptions:
+        # If the selection is in the list, run it's function passing
+        #the leagueData dictionary by default.
+        for option in football_options:
             if selection.lower() == "m":
-                exitMenu = True
+                exit_menu = True
                 break
             if selection == "7":
-                exitManualAnalysisMenu = False
-                while exitManualAnalysisMenu == False:
-                    anotherGame = ""
-                    newPrediction = []
-                    newPrediction = manualGameAnalysis(leagueData)
-                    if newPrediction == False:
+                exit_manual_analysis_menu = False
+                while not exit_manual_analysis_menu:
+                    another_game = ""
+                    new_prediction = []
+                    new_prediction = manual_game_analysis(league_data)
+                    if not new_prediction:
                         selection = ""
                         break
-                    if newPrediction != "" and newPrediction != []:
-                        predictions.append(newPrediction)
+                    if new_prediction != "" and new_prediction != []:
+                        predictions.append(new_prediction)
                         selection = ""
-                        while anotherGame.lower() != "y" and anotherGame.lower() != "n":
+                        while another_game.lower() != "y" and another_game.lower() != "n":
                             print("\nAnalyse another game? (Y/N)")
-                            anotherGame = input()
-                            if anotherGame.lower() == "n":
-                                exitManualAnalysisMenu = True
+                            another_game = input()
+                            if another_game.lower() == "n":
+                                exit_manual_analysis_menu = True
                                 break
-                            if anotherGame.lower() == "y":
-                                break
-                            
+                            if another_game.lower() == "y":
+                                break           
                 continue     
             if selection == "8":
-                reports(leagueData, predictions)
+                reports(league_data, predictions)
                 selection = ""
                 continue
             if selection == "9":
-                leagueData = importJSONFile()
+                league_data = import_json_file()
                 selection = ""
                 continue
             if selection == "10":
-                leagueData = {}
+                league_data = {}
                 selection = ""
                 continue
             
             # General action for other menu items
             if selection == option[1]:
-                option[2](leagueData)
+                option[2](league_data)
                 selection = ""
