89a75ed5ef8c8672d2373761a68b3f7f8cf57286
David Bristoll <36759641+Pringleman83@users.noreply.github.com>
2018-10-03 01:06:11 +0100

    Update commonFunctions.py
    Reinstated previous valid_input function as the last update wasn't able to check if a numeric string was in a list of integers. For future revision.

--- commonFunctions.py
+++ commonFunctions.py
@@ -1,6 +1,5 @@
 #Useful functions
 
-
 def is_number(s: str) -> bool:
     """
     Tests if the value passed is a number.
@@ -9,12 +8,14 @@ def is_number(s: str) -> bool:
     if isinstance(s, str):
         return s.isnumeric()
     return isinstance(s, int) or isinstance(s, float)
-
-
-def valid_input(selection, options: list) -> bool:
+    
+def valid_input(selection, options) -> bool:
     """
     Takes the user's selection and a list of valid options.
     Returns True if the selection is in the list.
     Returns False if it's not.
     """
-    return selection in options
+    for option in options:
+        if str(selection) == str(option):
+            return True    
+    return False
